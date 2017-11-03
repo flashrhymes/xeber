4c561a279c8ad88eb5493149848c383d5ed4bed1
flashrhymes <grandbossgrim@gmail.com>
2017-11-03 17:33:30 +0400

    xeber

---
+++ xeberlers/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace xeberlers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvnews = new System.Windows.Forms.DataGridView();
+            this.rtbcontent = new System.Windows.Forms.RichTextBox();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpdate = new System.Windows.Forms.DateTimePicker();
+            this.cmbcategories = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvnews)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvnews
+            // 
+            this.dgvnews.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvnews.Location = new System.Drawing.Point(350, 12);
+            this.dgvnews.Name = "dgvnews";
+            this.dgvnews.Size = new System.Drawing.Size(604, 428);
+            this.dgvnews.TabIndex = 0;
+            this.dgvnews.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvnews_RowHeaderMouseClick);
+            // 
+            // rtbcontent
+            // 
+            this.rtbcontent.Location = new System.Drawing.Point(12, 137);
+            this.rtbcontent.Name = "rtbcontent";
+            this.rtbcontent.Size = new System.Drawing.Size(317, 137);
+            this.rtbcontent.TabIndex = 1;
+            this.rtbcontent.Text = "";
+            // 
+            // txttitle
+            // 
+            this.txttitle.Location = new System.Drawing.Point(12, 28);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(226, 20);
+            this.txttitle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time";
+            // 
+            // dtpdate
+            // 
+            this.dtpdate.Location = new System.Drawing.Point(12, 89);
+            this.dtpdate.Name = "dtpdate";
+            this.dtpdate.Size = new System.Drawing.Size(226, 20);
+            this.dtpdate.TabIndex = 5;
+            // 
+            // cmbcategories
+            // 
+            this.cmbcategories.FormattingEnabled = true;
+            this.cmbcategories.Location = new System.Drawing.Point(12, 302);
+            this.cmbcategories.Name = "cmbcategories";
+            this.cmbcategories.Size = new System.Drawing.Size(226, 21);
+            this.cmbcategories.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Content";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Categories";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(12, 338);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(317, 32);
+            this.btnadd.TabIndex = 9;
+            this.btnadd.Text = "Add News";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(12, 388);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(317, 32);
+            this.btndelete.TabIndex = 10;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Visible = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(12, 338);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(317, 32);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Visible = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 452);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbcategories);
+            this.Controls.Add(this.dtpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txttitle);
+            this.Controls.Add(this.rtbcontent);
+            this.Controls.Add(this.dgvnews);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvnews)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvnews;
+        private System.Windows.Forms.RichTextBox rtbcontent;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpdate;
+        private System.Windows.Forms.ComboBox cmbcategories;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ xeberlers/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xeberlers.model1;
+
+namespace xeberlers
+{
+    public partial class Form1 : Form
+    {
+        private newsEntities db = new newsEntities();
+        private News selectedcategori;
+        public Form1()
+        {
+            InitializeComponent();
+            loadData();
+            dgvnews.Columns[0].Visible = false;
+            fillcombobox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadData()
+        {
+            dgvnews.DataSource = db.News.Select(n => new
+            {
+                n.Id,
+                n.Title,
+                n.Date,
+                n.Contents,
+                categores = n.Categories.Name
+
+            }).ToList();
+        }
+        private void fillcombobox()
+        {
+            List<Categories> list = db.Categories.ToList();
+            foreach(Categories item in list)
+            {
+                cmbcategories.Items.Add(item.Name);
+            }
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            string title = txttitle.Text;
+            DateTime date = dtpdate.Value;
+            string content = rtbcontent.Text;
+            string categoori = cmbcategories.Text;
+            if (title == string.Empty || content == string.Empty || categoori == string.Empty)
+            {
+                MessageBox.Show("Xanalari secin");
+                return;
+            }
+
+                News nw = new News();
+            nw.Title = title;
+            nw.Date = date;
+            nw.Contents = content;
+            int categoriId = this.getcategoriIdbyname(categoori);
+            nw.CategoriId = categoriId;
+            db.News.Add(nw);
+            db.SaveChanges();
+            loadData();
+            
+
+            
+
+        }
+        private int getcategoriIdbyname(string categori)
+        {
+            return db.Categories.FirstOrDefault(c => c.Name == categori).Id;
+        }
+
+        private void dgvnews_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+            int id = Convert.ToInt32(dgvnews.Rows[e.RowIndex].Cells[0].Value.ToString());
+            this.selectedcategori = db.News.Find(id);
+            txttitle.Text = this.selectedcategori.Title;
+            dtpdate.Value = (DateTime) this.selectedcategori.Date;
+            rtbcontent.Text = this.selectedcategori.Contents;
+            cmbcategories.Text = this.selectedcategori.Categories.Name;
+            btnadd.Visible = false;
+            btnUpdate.Visible = true;
+            btndelete.Visible = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+
+            selectedcategori.Title = txttitle.Text;
+            selectedcategori.Date = dtpdate.Value;
+            selectedcategori.Contents = rtbcontent.Text;
+            selectedcategori.Categories.Name = cmbcategories.Text;
+            db.SaveChanges();
+            reset();
+
+
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Eminisiniz mi?", "Sil", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                db.News.Remove(this.selectedcategori);
+                db.SaveChanges();
+                reset();
+            }
+        }
+        private void reset()
+        {
+            loadData();
+            txttitle.Text = "";
+            rtbcontent.Text = "";
+            cmbcategories.Text = "";
+            btnadd.Visible = true;
+            btnUpdate.Visible = false;
+            btndelete.Visible = false;
+        }
+    }
+}
